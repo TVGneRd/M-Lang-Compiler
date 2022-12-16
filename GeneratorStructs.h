c83df494c3b80bf52b309c7d7d34042cff34f581
Дмитрий <dimas.seledkin.2002@mail.ru>
2022-12-17 02:41:57 +0300

    Добавлены классы

--- GeneratorStructs.h
+++ GeneratorStructs.h
@@ -22,7 +22,6 @@ public:
 		this->node = node;
 	}
 
-	virtual void convolution() = 0;
 	virtual string assembly() = 0;
 
 private:
@@ -31,10 +30,6 @@ private:
 
 class Assigment: public Operation {
 public:
-	void convolution() {
-
-	}
-
 	string assembly() {
 		return "mov";
 	}
@@ -45,30 +40,109 @@ class Expression : public Operation {
 public:
 	string name;
 
-	void convolution() {
-		//node->getName()
-	}
-
 	string assembly() {
 		return "mov";
 	}
 
 };
 
+class PassedArguments : public Operation {
+public:
+
+	PassedArguments(Node* node) : Operation(node) {};
+
+	string assembly() {
+		string result = "push eax\n";
+		result += "mov eax, " + node->getFirst()->getToken().get_name() + "\n";
+		result += "push eax\n";
+
+		return result;
+	}
+
+};
+
+class LeftOperand : public Operation {
+public:
+
+	LeftOperand(Node* node) : Operation(node) {};
+
+	string assembly() {
+		Node *next = node->getFirst();
+
+		string result = "push eax\n";
+
+		if (next->getName() == "<ÂÛÇÎÂ>") {
+			Node* idToken = node->getFirst();
+
+			result += PassedArguments(node->getNext()[1]).assembly() + "\n";
+			result += "call " + idToken->getFirst()->getToken().get_name();
+
+			return result;
+		}
+
+		if (next->getName() == "<ÏÎÑËÅÄ. ÖÈÔĞ>") {
+			result += "move eax, " + node->getFirst()->getToken().get_name() + "\n";
+
+			return result;
+		}
+
+		if (next->getName() == "<ÈÄÅÍÒÈÔÈÊÀÒÎĞ>") {
+			result += "move eax, " + node->getFirst()->getToken().get_name() + "\n";
+
+			return result;
+		}
+	}
+
+};
+
+class RightOperand : public Operation {
+public:
+
+	RightOperand(Node* node) : Operation(node) {};
+
+	string assembly() {
+		Node* next = node->getFirst();
+
+		string result = "push ebx\n";
+
+		if (next->getName() == "<ÂÛÇÎÂ>") {
+
+
+			Node* idToken = node->getFirst();
+
+			result += "push eax\n";
+			result += PassedArguments(node->getNext()[1]).assembly() + "\n";
+			result += "call " + idToken->getFirst()->getToken().get_name() + "\n";
+			result += "mov ebx, eax\n";
+			result += "pop eax";
+
+			return result;
+		}
+
+		if (next->getName() == "<ÏÎÑËÅÄ. ÖÈÔĞ>") {
+			result += "move ebx, " + node->getFirst()->getToken().get_name() + "\n";
+
+			return result;
+		}
+
+		if (next->getName() == "<ÈÄÅÍÒÈÔÈÊÀÒÎĞ>") {
+			result += "move ebx, " + node->getFirst()->getToken().get_name() + "\n";
+
+			return result;
+		}
+	}
+
+};
 
 class LogicExpression : public Operation {
 public:
 
-	LogicExpression(Node* node) : Operation(node) {
-		convolution();
-	};
-
-	void convolution() {
-		//node->getName()
-	}
+	LogicExpression(Node* node) : Operation(node) {};
 
 	string assembly() {
-		return "";
+		string result = LeftOperand(node->getFirst()).assembly();
+
+		return result;
 	}
 
 };
@@ -76,13 +150,7 @@ public:
 class While : public Operation {
 public:
 
-	While(Node* node) : Operation(node) {
-		convolution();
-	};
-
-	void convolution() {
-		
-	}
+	While(Node* node) : Operation(node) {};
 
 	string assembly() {
 		string id1 = "while_begin_" + node->getId();
@@ -91,6 +159,8 @@ public:
 		string result = id1 + ":\n";
 		result += LogicExpression(node->getFirst()).assembly() + " " + id2;
 		result += id2 + ":\n";
+
+		return result;
 	}
 
 };
